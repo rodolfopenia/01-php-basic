440d17bd618184a92dc98c530b484974ebc1d114
Rodolfo Peña <rodolfo.penia@hotmail.com>
2024-06-20 02:23:00 -0500

    clase 03 variables y constantes

---
+++ 03 variables y constantes.php
@@ -0,0 +1,12 @@
+<?php
+
+$numero_1 = 8;
+$numero_2 = 7;
+
+echo $numero_1 + $numero_2 . "\n";
+
+// Constantes
+// Buena practica es definirla en mayuscula
+define("NUMERO_PI", 3.14);
+
+echo NUMERO_PI;
