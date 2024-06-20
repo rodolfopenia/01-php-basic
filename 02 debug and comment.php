ecefea689f487acd8aaa833e9b5df8cf615b386b
Rodolfo Peña <rodolfo.pena@apuestatotal.com>
2024-06-20 02:13:48 -0500

    clase 02 debugging and comments

---
+++ 02 debug and comment.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$personas = [
+    "Rodo" => 22,
+    "Mr. Ñuñi" => 15,
+    "Juan" => 65
+];
+
+// Primera forma de hacer debug
+var_dump($personas);
+
+// Segunda forma (y mejor) de hacer debug
+print_r($personas);
+
+// Los comentarios se pueden hacer con dos slash
+// sirve para dejar notas
+
+# También se puede usar el michi
+# Esto es un comentario
+
+/*
+Esto es otra forma de dejar un comentario por bloques
+En varias líneas
+*/
