5584ee82b61da64a7df8a841f63db7f79306d1b9
Rodolfo Peña <rodolfo.penia@hotmail.com>
2024-06-20 08:30:00 -0500

    Clase 07 operadores aritméticos

---
+++ 07 operadoresaritmeticos.php
@@ -0,0 +1,21 @@
+<?php
+
+$resultado = 5 -3;
+
+echo 5 + 5 . "\n";
+echo 5 - 5 . "\n";
+
+echo $resultado . "\n";
+
+echo 5 ** 3 . "\n";
+
+echo (5 * 6) . " ". (80 / 4) . "\n";
+
+echo 125 / 5 . "\n";
+echo 125 % 5 . "\n";
+
+$horas = 7201;
+echo "$horas segundos convertidos a horas son " . ($horas / 3600) . "\n";
+echo "Sobran " . $horas % 3600 . "\n";
+
+echo "\n";
