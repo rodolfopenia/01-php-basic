b355d6a17af8a5be0a67c2fee0904f39fff4dc9e
Rodolfo Peña <rodolfo.penia@hotmail.com>
2024-06-20 03:11:14 -0500

    clase 06 operadores logicos

---
+++ 06 operadoreslogicos.php
@@ -0,0 +1,25 @@
+<?php
+
+$michis_felinos = true;
+$michis_4_patas = true;
+$michis_vuelan = false;
+$michis_programan_con_PHP = false;
+
+// And
+
+var_dump($michis_felinos && $michis_4_patas);
+
+// Or
+var_dump($michis_4_patas || $michis_vuelan);
+
+// Not
+var_dump(!$michis_4_patas);
+
+// Experimentos
+// Se puede usar && or and
+// Sale true porque el igual tiene prioridad sobre and
+// Recomendación, siempre usar &&
+$resultado = $michis_4_patas and $michis_programan_con_PHP;
+var_dump($resultado);
+
+echo "\n";
